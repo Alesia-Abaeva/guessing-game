eb048a1850e380ae6d37b18d4ee2833ef8565090
Alesia <alesia.litvinenko@gmail.com>
2022-08-15 17:43:05 +0300

    fead: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,46 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minNumber
+        this.maxNumber
+        this.result
+    }
+    // инициализируем специальные значения этого класса
 
     setRange(min, max) {
+        this.minNumber = min
+
+
+
+        this.maxNumber = max
+
 
     }
+
 
     guess() {
+        this.result =  Math.round((this.maxNumber + this.minNumber) / 2)
+       return this.result
 
     }
+
 
     lower() {
 
+        this.maxNumber = Math.round((this.maxNumber + this.minNumber) / 2)
+
+        
     }
+
+
 
     greater() {
 
+       this.minNumber =  Math.round((this.maxNumber + this.minNumber) / 2)
     }
+ 
 }
 
 module.exports = GuessingGame;
+
+
+
